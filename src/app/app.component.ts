a9b8de5cd083fdccd5100b62eeb3b8342f28931a
Eskantu <eskantu@hotmail.com>
2024-11-18 21:28:06 -0600

    add flowbite and prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginComponent } from './Pages/login/login.component';
+import { UploadFileComponent } from './Pages/upload-file/upload-file.component';
+import { FlowbiteService } from '../app/Services/flowbite.service';
+interface City {
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, UploadFileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'learning-hub';
+  constructor(private flowbiteService: FlowbiteService) {}
+  ngOnInit() {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
 }

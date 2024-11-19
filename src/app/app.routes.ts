e9e284e1db55d74ef8ccce3874cdd7e3344a3ff1
Eskantu <eskantu@hotmail.com>
2024-11-18 22:32:25 -0600

    Add routing for login, upload, home, and page not found components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { UploadFileComponent } from './Pages/upload-file/upload-file.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'Login',
+    component: LoginComponent,
+  },
+  {
+    path: 'Upload',
+    component: UploadFileComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  { path: '**', component: PageNotFoundComponent },
+];

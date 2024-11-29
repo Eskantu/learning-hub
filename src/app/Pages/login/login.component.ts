9447d50446a52d38eb27606303ed881cc5083d38
Eskantu <eskantu@hotmail.com>
2024-11-28 20:33:25 -0600

    Refactor login component to use reactive forms and improve layout

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { Button } from 'primeng/button'
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../Services/Authentication/authentication.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardModule,Button, ReactiveFormsModule],
+  imports: [CardModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  loginForm = new FormControl('');
-  submitted = false;
-  /**
-   *
-   */
-  constructor(private formBuilder:FormBuilder) {
-  }
-  ngOnInit(): void {
-  }
+  constructor(private router: Router, private AuthService:AuthenticationService) {}
+  ngOnInit(): void {}
 
+  fnLogin() {
+    this.router.navigate(['/Home']);
+    this.AuthService.setFakeToken('fakeToken');
+  }
 }

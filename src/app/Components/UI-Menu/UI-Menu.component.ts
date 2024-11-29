9b86e126da43063f45243a62c982f9cca2d8cf34
Eskantu <eskantu@hotmail.com>
2024-11-28 20:32:07 -0600

    Add UI Menu component with styles and change detection strategy

---
+++ src/app/Components/UI-Menu/UI-Menu.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './UI-Menu.component..html',
+  styleUrl: './UI-Menu.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UIMenuComponent { }

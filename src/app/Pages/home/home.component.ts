d93ddd5cef63018e62a5fe90e31adc4c7596c60a
Eskantu <eskantu@hotmail.com>
2024-11-28 20:33:51 -0600

    Import UIModalComponent in home component for enhanced functionality

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { UIModalComponent } from '../../Components/ui-modal/ui-modal.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [ UIModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

2332726095c37a3721ee5964a3812a01b7451e2a
Eskantu <eskantu@hotmail.com>
2024-11-28 20:34:08 -0600

    Add UI components for top bar and menu, enhance layout with authentication check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 import { ButtonModule } from 'primeng/button';
+import { UIMenuComponent } from './Components/UI-Menu/UI-Menu.component'
+import { UITopBarComponent } from './Components/ui-top-bar/ui-top-bar.component';
+import { CommonModule } from '@angular/common';
+import { AuthenticationService } from './Services/Authentication/authentication.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  ButtonModule],
+  imports: [RouterOutlet, UIMenuComponent, UITopBarComponent,  ButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(public AuthService:AuthenticationService) { }
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
   title = 'Learnning-Hub';
 }
